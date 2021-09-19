5b901ed7e104f0b7d30241fcb9ab09f571e65709
maito094 <mlbozhkov@gmail.com>
2021-09-19 19:54:19 +0300

    Added Csharp_AdvancedOOP_Exercises : Stacks and Queues

---
+++ C_Sharp_Advanced_OOP/Csharp_AdvancedOOP_Exercises/Stacks and Queues - Ex/Stacks_and_Queues_Ex/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int[] numOperations = Console.ReadLine()
+                                     .Split(" ")
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+         int[] arrayNum = Console.ReadLine()
+                                     .Split(" ")
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+
+         Stack<int> numbers = new Stack<int>();
+
+         int elementsToPush = numOperations[0];
+         int elementsToPop = numOperations[1];
+         int elementToFind = numOperations[2];
+
+
+         for (int i = 0; i < elementsToPush; i++)
+         {
+            numbers.Push(arrayNum[i]);
+         }
+         for (int i = 0; i < elementsToPop; i++)
+         {
+            numbers.Pop();
+         }
+
+
+         if (numbers.Count == 0)
+         {
+            Console.WriteLine(0);
+
+            Environment.Exit(0);
+         }
+
+         int smallest = int.MaxValue;
+         bool isFound = false;
+
+         foreach (var number in numbers)
+         {
+
+            if (elementToFind == number)
+            {
+               isFound = true;
+            }
+
+            if (number < smallest)
+            {
+               smallest = number;
+            }
+
+         }
+         Console.WriteLine(isFound ? isFound.ToString().ToLower() : smallest.ToString());
+      }
+
+
+   }
+
+}
---
+++ C_Sharp_Advanced_OOP/Csharp_AdvancedOOP_Exercises/Stacks and Queues - Ex/Stacks_and_Queues_Ex/06. Songs Queue/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+
+
+         while (true)
+         {
+            string command = Console.ReadLine();
+
+
+            if (command.Contains("Play"))
+            {
+
+               songs.Dequeue();
+
+            }
+            else if (command.Contains("Add"))
+            {
+               string[] helpstring = command.Split("Add ");
+
+
+               string nameSong = helpstring[1];
+               if (songs.Contains(nameSong))
+               {
+                  Console.WriteLine($"{nameSong} is already contained!");
+               }
+               else
+               {
+                  songs.Enqueue(nameSong);
+
+               }
+
+            }
+            else if (command.Contains("Show"))
+            {
+               Console.Write(string.Join(", ", songs));
+               Console.WriteLine();
+            }
+
+            if (songs.Count < 1)
+            {
+               Console.WriteLine("No more songs!");
+               break;
+
+            }
+
+         }
+
+      }
+   }
+}
