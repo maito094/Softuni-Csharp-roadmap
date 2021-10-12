307f8d27f172ea553be447ee3e3eb34f1876dc09
maito094 <mlbozhkov@gmail.com>
2021-10-12 20:41:36 +0300

    Added Csharp_AdvancedOOP_Labs : DoublyLinkedList.cs

---
+++ C_Sharp_Advanced_OOP/Csharp_AdvancedOOP_Labs/Linked Lists/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoublyLinkedList
+{
+   public class DoublyLinkedList
+   {
+      private LinkedListItem first = null;
+      private LinkedListItem last = null;
+
+      public int Count
+      {
+         get
+         {
+            var count = 0;
+            var current = first;
+            while (current != null)
+            {
+               count++;
+               current = current.Next;
+            }
+            
+            return count;
+         }
+      }
+
+      //– adds an element at the beginning of the collection
+      public void AddFirst(int element)
+      {
+         var newItem = new LinkedListItem(element);
+         if (first == null)
+         {
+            first = newItem;
+            last = newItem;
+         }
+         else
+         {
+            newItem.Next = first;
+            first.Previous = newItem;
+            first = newItem;
+         }
+
+      }
+      //– adds an element at the end of the collection
+      public void AddLast(int element)
+      {
+         var newItem = new LinkedListItem(element);
+         if (last == null)
+         {
+            first = newItem;
+            last = newItem;
+         }
+         else
+         {
+            last.Next = newItem;
+            newItem.Previous = last;
+            last = newItem;
+         }
+
+      }
+      //– removes the element at the beginning of the collection
+      public int RemoveFirst()
+      {
+         if (first == null)
+         {
+            //TODO : Exceptions
+            // Forn now Do nothing
+            return 0;
+         }
+         var currentFirstValue = first.Value;
+         if (first == last) //1 item
+         {
+            first = null;
+            last = null;
+         }
+         else  // more than 1 item
+         {
+            var newFirst = first.Next;
+            newFirst.Previous = null;
+            first = newFirst;
+         }
+
+         return currentFirstValue;
+      }
+      //– removes the element at the end of the collection
+      public int RemoveLast()
+      {
+         if (last == null)
+         {
+            //TODO : Exceptions
+            // Forn now Do nothing
+            return 0;
+         }
+         var currentLastValue = last.Value;
+         if (first == last) //1 item
+         {
+            first = null;
+            last = null;
+         }
+         else  // more than 1 item
+         {
+            var newLast = last.Previous;
+            newLast.Next = null;
+            last = newLast;
+         }
+
+         return currentLastValue;
+      }
+      //– goes through the collection and executes a given action
+      public void ForEach(Action<int> action)
+      {
+         var current = first;
+         while (current != null)
+         {
+            action(current.Value);
+            current = current.Next;
+         }
+      }
+      // – returns the collection as an array
+      public int[] ToArray()
+      {
+         var array = new int[Count];
+
+         var current = first;
+         var i = 0;
+         while (current != null)
+         {
+            array[i] = current.Value;
+            current = current.Next;
+            i++;
+         }
+         return array;
+      }
+
+   }
+
+
+}
---
+++ C_Sharp_Advanced_OOP/Csharp_AdvancedOOP_Labs/Linked Lists/DoublyLinkedList/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+
+
+
+
+   public class Program
+
+   {
+      static void Main(string[] args)
+      {
+         var list = new DoublyLinkedList();
+         list.AddFirst(3);
+         // 3
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddFirst(2);
+         // 2-3                         
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddFirst(1);
+         // 1-2-3                      
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddLast(4);
+         // 1-2-3-4                     
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddFirst(0);
+         // 0-1-2-3-4                   
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddLast(100);
+         // 0-1-2-3-4-100               
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.RemoveFirst();
+         // 1-2-3-4-100                
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.RemoveLast();
+         // 1-2-3-4                     
+         Console.WriteLine(string.Join("-", list.ToArray()));
+
+         list.AddLast(5);
+         //1-2-3-4-5
+         Console.WriteLine(list.Count);
+         Console.WriteLine(string.Join(", ", list.ToArray()));
+
+         list.ForEach(PrintToConsole);
+         //list.ForEach(x => Console.WriteLine(x));
+         //list.ForEach(Console.WriteLine()); it takes void method action
+      }
+      static void PrintToConsole(int i) => Console.WriteLine(i);
+   }
+}
